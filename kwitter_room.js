adfdbea160dfcb0af6a6add2ebb3fc09c7e12855
Riddhipatel02 <74219262+Riddhipatel02@users.noreply.github.com>
2021-05-24 21:35:26 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCgxhlsQGzRJAU045aiuLvGkj2Du8k6Yac",
+    authDomain: "kwitter-44b5e.firebaseapp.com",
+    databaseURL: "https://kwitter-44b5e-default-rtdb.firebaseio.com",
+    projectId: "kwitter-44b5e",
+    storageBucket: "kwitter-44b5e.appspot.com",
+    messagingSenderId: "898212606038",
+    appId: "1:898212606038:web:226064c160f75a3c720bf7"
+  };
